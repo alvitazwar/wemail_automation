31905715762901074032e155c248b3e2271c9c3b
alvitazwar <alvitazwar@wedevs.com>
2020-10-15 16:26:25 +0600

    help menu documentation

---
+++ help_01_documentation_test.js
@@ -0,0 +1,20 @@
+Feature('wemail_Helpmenu');
+
+Scenario('link check documentation', (I) => {
+  //I.loginAsAdmin();
+  I.amOnPage('https://stagingwp.getwemail.io/wp-admin');
+  //I.see('GitHub');
+  I.fillField('Username','alvitazwar');
+  I.fillField('Password','password');
+  I.click('Log In');
+  // /I.refreshPage();
+  //pause();
+  I.seeElement({xpath: '//*[@id="wpbody-content"]/div[3]/h1'});
+  I.waitForElement('//*[@id="toplevel_page_wemail"]/a/div[3]', 30);
+  //I.amOnPage('https://stagingwp.getwemail.io/wp-admin/admin.php?page=wemail#/');
+  I.click('//*[@id="toplevel_page_wemail"]/a/div[3]');
+  I.amOnPage('https://stagingwp.getwemail.io/wp-admin/admin.php?page=wemail#/help/support');
+  I.click('documentation');
+  
+
+});
